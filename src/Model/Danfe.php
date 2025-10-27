0b704508d29975381d3f877eca56d99ba6b2236c
GS-LucasCarvalho <Lucas.carvalho@giusoft.com.br>
2025-10-27 09:37:28 -0300

    Corrige model do danfe e refatora código para atender a nova arquitetura

--- src/Model/Danfe.php
+++ src/Model/Danfe.php
@@ -1,32 +1,29 @@
 <?php
 
-namespace App\Controller;
+namespace App\Model;
 
 use NFePHP\DA\NFe\Danfe as NFeDanfe;
 
 class Danfe
 {
-    private $tools;
+    private $corpoRequisicao;
     private $config;
+
+    public function __construct($dados)
+    {
+        $this->corpoRequisicao = $dados->corpoRequisicao;
+        $this->config = $dados->config;
+    }
 
     public function gerarDanfe()
     {
         try {
-            $dados = json_decode(file_get_contents('php://input'), true);
-
-            if (empty($dados['cnpj_emitente'])) {
-                throw new \Exception('O campo "cnpj_emitente" é obrigatório.');
-            }
-            $this->carregarEmpresas($dados['cnpj_emitente']);
-
-            $chave = $dados['chave'];
+            $chave = $this->corpoRequisicao['chave'];
             $cnpjLimpo = soNumeros($this->config['cnpj']);
-            $xmlPath = __DIR__ . "/notas/{$cnpjLimpo}/autorizadas/{$chave}-nfe.xml";
+            $xmlPath = __DIR__ . "/../storage/notas/{$cnpjLimpo}/autorizadas/{$chave}-nfe.xml";
 
             if (!file_exists($xmlPath)) {
-                http_response_code(404);
-                echo json_encode(['erro' => 'XML não encontrado']);
-                return;
+                emitirErro("XML não encontrado", 404);
             }
 
             $xml = file_get_contents($xmlPath);
@@ -34,18 +31,13 @@ class Danfe
             $danfe = new NFeDanfe($xml);
             $pdf = $danfe->render();
 
-            $pdfPath = __DIR__ . "/storage/notas/{$cnpjLimpo}/autorizadas/{$chave}-danfe.pdf";
+            $pdfPath = __DIR__ . "/../storage/notas/{$cnpjLimpo}/autorizadas/{$chave}-danfe.pdf";
             file_put_contents($pdfPath, $pdf);
 
-            http_response_code(200);
-            echo json_encode([
-                'success' => true,
-                'pdf' => base64_encode($pdf)
-            ]);
+            emitirSucesso(base64_encode($pdf), 200);
 
         } catch (\Exception $e) {
-            http_response_code(500);
-            echo json_encode(['erro' => $e->getMessage()]);
+            emitirErro($e->getMessage(), 500);
         }
     }
 }
