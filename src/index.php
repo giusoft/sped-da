8e74102ce485d34683411c7cb6e7b9e2e670f961
GS-LucasCarvalho <Lucas.carvalho@giusoft.com.br>
2025-10-30 17:17:05 -0300

    Atualiza index com novo controller

--- src/index.php
+++ src/index.php
@@ -3,38 +3,16 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 require_once __DIR__ . '/Lib/utils.php';
 
-use App\Controller\Nfe;
-use App\Controller\Danfe;
-use App\Controller\Sefaz;
+use App\Controller\Api;
 
-$requestUri = $_SERVER['REQUEST_URI'];
-$path = parse_url($requestUri, PHP_URL_PATH);
+$path = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
 
-// Verifica se o path tem nfe
 if ($path) {
 
-    if (strpos($path, '/nfe/') === 0) {
-        try {
-            $controller = new Nfe();
-        } catch (\Exception $e) {
-            emitirErro("Erro interno ao iniciar API", 500, $e->getMessage());
-        }
-    }
-
-    if (strpos($path, '/danfe/') === 0) {
-        try {
-            $controller = new Danfe();
-        } catch (\Exception $e) {
-            emitirErro("Erro interno ao iniciar API", 500, $e->getMessage());
-        }
-    }
-
-    if (strpos($path, '/sefaz/') === 0) {
-        try {
-            $controller = new Sefaz();
-        } catch (\Exception $e) {
-            emitirErro("Erro interno ao iniciar API", 500, $e->getMessage());
-        }
+    try {
+        $controller = new Api();
+    } catch (\Exception $e) {
+        emitirErro("Erro interno ao iniciar API", 500, $e->getMessage());
     }
 
 } else {
