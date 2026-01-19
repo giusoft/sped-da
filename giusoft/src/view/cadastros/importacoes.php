d005dd4f7aa180dee20fc88b722b98006e77d0b5
GS-LucasCarvalho <Lucas.carvalho@giusoft.com.br>
2026-01-19 16:57:20 -0300

    Refatora o código para o PHP 8.4

--- giusoft/src/view/cadastros/importacoes.php
+++ giusoft/src/view/cadastros/importacoes.php
@@ -1,24 +1,24 @@
 <?
 include_once $gPathDefault . "gUI.php";
 
-define(DOWNLOAD_MODELO, 100);
+define('DOWNLOAD_MODELO', 100);
 
 $ui = new gUI("{title: Importações de dados; table: importacoes; permissions: SIUD; columns: 2}");
+
 if ($gId) {
     $ui->addButton("{style: info; title: Baixar modelo CSV; hint: Baixar modelo de importação; icon: download; href: " . $o->page . '&gPage=' . DOWNLOAD_MODELO . "&gId=" . $gId . "}");
 }
+
 $ui->addTable("{title: Campos para o cabecalho; name: importacoes_cabecalho; foreignKey: id_importacoes; relationship: one-to-many; permissions: SIUD}");
 $ui->addTable("{title: Campos para os registros; name: importacoes_registros; foreignKey: id_importacoes; relationship: one-to-many; permissions: SIUD}");
 $ui->addDictionary("{name: tipo; fieldLabel: Tipo; type: combo; items: {'Texto','Texto maiúsculas','Número inteiro','Número decimal','Data','Data hora','Lógico', 'CNPJ','CPF','Carácter inicial','Unidade do Item'}}");
 $ui->addDictionary("{name: validacao; fieldLabel: Validação; type: lowerText; allowBlank: true}");
 $ui->addDictionary("{name: id_pessoas_proprietario; fieldLabel: Proprietário; type:combo; items: ".$sp['combo_proprietarios']."; allowBlank: true}");
-
-//$grupos=array("progracao_entrada" => "Entrada","programacao_saida" => "Saída","Itens" => "itens");
 $ui->addDictionary("{name: grupo; fieldLabel: Grupo; type:combo; items:'{programacao_entrada,programacao_saida,itens}'; allowBlank: false}");
 $ui->run($o, $html);
 
 switch($gPage) {
-case DOWNLOAD_MODELO:
-    downloadModeloImportacao('', $gId);
-    break;
+    case DOWNLOAD_MODELO:
+        downloadModeloImportacao('', $gId);
+        break;
 }
