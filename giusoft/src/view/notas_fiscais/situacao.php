5011a2ccbbf86778349516fbe7c8f42e5c0ce90b
GS-LucasCarvalho <Lucas.carvalho@giusoft.com.br>
2026-01-20 15:44:19 -0300

    Refatora para funcionar no PHP 8.4

--- giusoft/src/view/notas_fiscais/situacao.php
+++ giusoft/src/view/notas_fiscais/situacao.php
@@ -1,102 +1,97 @@
-<?
-define('INICIO'                                 ,0);
-define('DADOS'                                  ,1);
-define('PESQUISAR'                              ,10);
-define('PESQUISAR_RESULTADO'    ,11);
-define('NOVO'                                   ,20);
-define('NOVO_SALVAR'                    ,21);
-define('EXCLUIR'                                ,30);
+<?php
+
+define('INICIO'              , 0);
+define('DADOS'               , 1);
+define('PESQUISAR'           , 10);
+define('PESQUISAR_RESULTADO' , 11);
+define('NOVO'                , 20);
+define('NOVO_SALVAR'         , 21);
+define('EXCLUIR'             , 30);
+
 $html.=$o->msgTitle("Situação");
+
 $tabela = "areas";
-switch($gPage)
-{
+
+switch($gPage) {
         case INICIO:
-        	$html.='<div class="hidden-print"><form class="form-inline" method="POST" action="index.php?g=painel">';
-            $html.='<input id="pesquisa" name="pesquisa" type="text" class="form-control input-md" placeholder="Pesquisa rápida...">&nbsp;<input type="hidden" name="g" value="painel"><input type="hidden" name="gPage" value="0"> ';
-            $html.='<input id="action" name="action" type="hidden" class="form-control input-md" value="filtro">';
-            $html.=$o->button("{icon: search; caption: Pesquisar; hint: Pesquisa Avançada; size: normal; href: index.php?g=painel&gPage=".PESQUISAR."}");
-            $html.='</form></div>';
+        	$html .= '<div class="hidden-print"><form class="form-inline" method="POST" action="index.php?g=painel">';
+            $html .= '<input id="pesquisa" name="pesquisa" type="text" class="form-control input-md" placeholder="Pesquisa rápida...">&nbsp;<input type="hidden" name="g" value="painel"><input type="hidden" name="gPage" value="0"> ';
+            $html .= '<input id="action" name="action" type="hidden" class="form-control input-md" value="filtro">';
+            $html .= $o->button("{icon: search; caption: Pesquisar; hint: Pesquisa Avançada; size: normal; href: index.php?g=painel&gPage=".PESQUISAR."}");
+            $html .= '</form></div>';
             $html .= $o->br();
+
             // Botões com ações possíveis...
         	$frm = new gForm("columns: 3");
-            if ($_POST["action"] == "filtro")
-            {
+            if ($_POST["action"] == "filtro") {
                 /* TRATAR FILTRO */
-                $sql = "select * from $tabela";
+                $sql = "SELECT * FROM " . $tabela;
                 $rs = dbQuery($sql);
-            } else
-            {
-                $sql = "select * from $tabela";
+            } else {
+                $sql = "SELECT * FROM " . $tabela;
                 $rs = dbQuery($sql);
             }
-            $html.=mostraTabelaPrincipal($rs);
-        break;
-        case PESQUISAR:
-                $frm=new gForm();
-                $frm->addFormMessage("Informe uma ou mais opções abaixo para busca...");
-                $frm->add("{name: campo}");
-                $html.=$frm->render($o);
-        break;
-        case PESQUISAR_RESULTADO:
-                /*
-                $campo = gCleanField($_REQUEST['campo']);
-                $campo = intval($_REQUEST['campo']);
-                $campo = gDBDate($_REQUEST['campo']);
-                $campo = gDBDateTime($_REQUEST['campo']);
-                $campo = gDBFloat($_REQUEST['campo']);
-                $campo = gDBCheck($_REQUEST['campo']);
 
-                // Monta a query de acordo com os filtros
-                $filtros = '';
-                $where   = '';
-                $order   = '';
-                */
-                $html.=mostraTabelaPrincipal($rs);
-        break;
-        case NOVO:
-                $frm=new gForm();
+            $html .= mostraTabelaPrincipal($rs);
+            break;
+
+        case PESQUISAR:
+                $frm = new gForm();
                 $frm->addFormMessage("Informe uma ou mais opções abaixo para busca...");
                 $frm->add("{name: campo}");
-                $html.=$frm->render($o);
-        break;
+                $html .= $frm->render($o);
+            break;
+
+        case PESQUISAR_RESULTADO:
+            $html.=mostraTabelaPrincipal($rs);
+            break;
+
+        case NOVO:
+            $frm=new gForm();
+            $frm->addFormMessage("Informe uma ou mais opções abaixo para busca...");
+            $frm->add("{name: campo}");
+            $html.=$frm->render($o);
+            break;
+
         case NOVO_SALVAR:
-                // Trata os campos obtidos do formulário 
-                $campos = array();
-                $campos['campo'] = gCleanField($_REQUEST['campo']);
-                $campos['campo'] = intval($_REQUEST['campo']);
-                $campos['campo'] = gDBDate($_REQUEST['campo']);
-                $campos['campo'] = gDBDateTime($_REQUEST['campo']);
-                $campos['campo'] = gDBFloat($_REQUEST['campo']);
-                $campos['campo'] = gDBCheck($_REQUEST['campo']);
-                redirect($o->page);
-        break;
+            // Trata os campos obtidos do formulário
+            $campos = [];
+            $campos['campo'] = gCleanField($_REQUEST['campo']);
+            $campos['campo'] = intval($_REQUEST['campo']);
+            $campos['campo'] = gDBDate($_REQUEST['campo']);
+            $campos['campo'] = gDBDateTime($_REQUEST['campo']);
+            $campos['campo'] = gDBFloat($_REQUEST['campo']);
+            $campos['campo'] = gDBCheck($_REQUEST['campo']);
+            redirect($o->page);
+            break;
         case EXCLUIR:
-                $sql = "DELETE FROM tabela WHERE id=".$gId;
-                dbQuery($sql);
-                redirect($o->page);
-        break;
+            $sql = "DELETE FROM tabela WHERE id=".$gId;
+            dbQuery($sql);
+            redirect($o->page);
+            break;
 }
 
 function mostraTabelaPrincipal($rs)
 {
 	global $o;
-    if (count($rs)>0)
-    {
+
+    if ($rs) {
         $html.=$o->tableBegin("big",true);
-        $mtz = array();
-        $mtz[]="<-Opções";
-        $mtz[]="<-Descrição";
-        $html.=$o->tableRow($mtz,"header");
-        foreach ($rs as $row)
-        {
-            $mtz = array();
+        $mtz = [];
+        $mtz[] = "<-Opções";
+        $mtz[] = "<-Descrição";
+        $html .= $o->tableRow($mtz,"header");
+        foreach ($rs as $row) {
+            $mtz = [];
             $mtz[]="<-" . $o->button("{icon: folder-open; caption: Executar; hint: Editar informações; style: default; size: tiny; href: ". $o->page ."&gPage=1&gId=". $row["id"] ."}");
             $mtz[]="<-".$row['descricao'];
             $html.=$o->tableRow($mtz,"detail");
         }
-        $html.=$o->tableEnd();
+
+        $html .= $o->tableEnd();
     } else {
-        $html.=$o->msgInfo("Nenhum registro encontrado");
-    }  
-    return ($html);    
+        $html .= $o->msgInfo("Nenhum registro encontrado");
+    }
+
+    return ($html);
 }
