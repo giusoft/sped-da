7e4cf4d6644c8f126dc52ad7af57140a54addb7a
GS-LucasCarvalho <Lucas.carvalho@giusoft.com.br>
2026-01-20 14:18:55 -0300

    Refatora para funcionar no PHP 8.4

--- giusoft/src/view/system/parameters.php
+++ giusoft/src/view/system/parameters.php
@@ -1,62 +1,52 @@
-<?
+<?php
+
 include_once $gPathDefault . "gUI.php";
 
-define(QUESTIONARIO_DETALHADO,100);
-define(QUESTIONARIO_RESUMIDO,200);
+define('QUESTIONARIO_DETALHADO' ,100);
+define('QUESTIONARIO_RESUMIDO'  ,200);
 
-if ($usrId > 1 && $gPage==3)
-{
+if ($usrId > 1 && $gPage == 3 && $gId > 0) {
+    /*
+    Impede que usuários alterem o valor de parâmetros não editáveis.
+    Isso DEVE ser feito antes da instanciação da classe gUI
 
-    if($gId > 0)
-    {
-        /*
-        Impede que usuários alterem o valor de parâmetros não editáveis.
-        Isso DEVE ser feito antes da instanciação da classe gUI
+    @author André Luiz em 18-05-2017 17:00
+    */
+    $sql = "SELECT editavel,valor, ativo FROM parametros WHERE id = " . $gId;
+    $rs = dbQuery($sql);
+    if (intval($rs[0]['editavel']) == 0) {
+        $_GET['valor'] = $rs[0]['valor'];
+        $_POST['valor'] = $rs[0]['valor'];
+        $_REQUEST['valor'] = $rs[0]['valor'];
 
-        @author André Luiz em 18-05-2017 17:00
-        */
-        $sql = "SELECT editavel,valor, ativo FROM parametros WHERE id = $gId";
-        $rs = dbQuery($sql);
-
-        if(intval($rs[0]['editavel']) == 0)
-        {
-            $_GET['valor'] = $rs[0]['valor'];
-            $_POST['valor'] = $rs[0]['valor'];
-            $_REQUEST['valor'] = $rs[0]['valor'];
-
-            $_GET['editavel'] = $rs[0]['editavel'];
-            $_POST['editavel'] = $rs[0]['editavel'];
-            $_REQUEST['editavel'] = $rs[0]['editavel'];
-        }
+        $_GET['editavel'] = $rs[0]['editavel'];
+        $_POST['editavel'] = $rs[0]['editavel'];
+        $_REQUEST['editavel'] = $rs[0]['editavel'];
     }
 }
 
 
-if ($usrId>2)
-{
+if ($usrId > 2) {
     $html.=$o->msgDanger("Acesso negado a esta funcionalidade<br>Somente o administrador do sistema tem direito a acessar esta funcionalidade");
-} else
-{
-    if ($usrId==1)
-    {
+} else {
+    if ($usrId == 1) {
         $ui = new gUI("{title: Parâmetros; columns: 2; table: parametros; permissions: SIU; ajax: false}");
         $ui->addDictionary("{name: editavel; fieldLabel: Editável}");
-    } else
-    {
+    } else {
         $ui = new gUI("{title: Parâmetros; columns: 2; table: parametros; permissions: SU; ajax: false}");
         $ui->addDictionary("{name: descricao; type: show}");
         $ui->addDictionary("{name: ajuda; type: show}");
         $ui->addDictionary("{name: editavel; fieldLabel: Editável; type: hidden}");
     }
+
     $ui->addButton("{icon: question; title: Gerar questionário completo; href: ".$o->page."&gPDFOrientation=L&gPage=".QUESTIONARIO_DETALHADO."}");
     $ui->addButton("{icon: question; title: Gerar questionário resumido; href: ".$o->page."&gPage=".QUESTIONARIO_RESUMIDO."}");
     $ui->run($o, $html);
 
-    $sql="SELECT * FROM parametros";
-    $rsp=dbQuery($sql);
-    $gParam='';
-    foreach ($rsp as $key=>$value)
-    {
+    $sql = "SELECT * FROM parametros";
+    $rsp = dbQuery($sql);
+    $gParam = '';
+    foreach ($rsp as $key => $value) {
         unset($value[0]);
         unset($value[1]);
         unset($value[2]);
@@ -65,71 +55,69 @@ if ($usrId>2)
         unset($value[5]);
         $gParam[$value['chave']]=$value;
     }
+
     $_SESSION['gParam']=$gParam;
 
 }
 
-if ($gPage==QUESTIONARIO_DETALHADO)
-{
+if ($gPage == QUESTIONARIO_DETALHADO) {
     $o->PDFEnabled = true;
     $o->CSVEnabled = true;
     $o->XLSEnabled = true;
-    $html.=$o->msgTitle("Parâmetros");
-    $html.=$o->msgSubTitle("Questionário para parametrização do sistema");
+    $html .= $o->msgTitle("Parâmetros");
+    $html .= $o->msgSubTitle("Questionário para parametrização do sistema");
     $sql = "SELECT * FROM parametros order by grupo,chave";
-    $rs=dbQuery($sql);
-    $html.=$o->tableBegin("big", true);
-    $mtz = array();
-    $mtz[]="->Id";
-    $mtz[]="<-Grupo  ";
-    $mtz[]="<-Chave            ";
-    $mtz[]="<-Item                                                 ";
-    $mtz[]="<>Ativar   ";
-    $mtz[]="<-Valor padrão";
-    $mtz[]="<-Resposta            ";
-    $html.=$o->tableRow($mtz, "header");
-    foreach ($rs as $row)
-    {
-        $mtz = array();
+    $rs = dbQuery($sql);
+    $html .= $o->tableBegin("big", true);
+    $mtz = [];
+    $mtz[] = "->Id";
+    $mtz[] = "<-Grupo";
+    $mtz[] = "<-Chave";
+    $mtz[] = "<-Item";
+    $mtz[] = "<>Ativar";
+    $mtz[] = "<-Valor padrão";
+    $mtz[] = "<-Resposta";
+    $html .= $o->tableRow($mtz, "header");
+    foreach ($rs as $row){
+        $mtz = [];
         $mtz[]="->".$row["id"];
         $mtz[]="<-".$row["grupo"];
         $mtz[]="<-".$row["chave"];
-        if ($_REQUEST['gPDF']==1)
-        {
-        //$mtz[]="<-".str_replace(", ",",\n",str_replace(". ",".\n",$row['ajuda']));
-            $mtz[]="<-".$row['descricao'];
-            $mtz[]="<>".($row["ativo"]==1?'[  ]<b>SIM</b>   [  ]não':'[  ]Sim   [  ]<b>NÃO</b>');
-            $mtz[]="<-".$row["valor"]." ";
+        if ($_REQUEST['gPDF'] == 1) {
+            $mtz[] = "<-".$row['descricao'];
+            $mtz[] = "<>".($row["ativo"]==1?'[  ]<b>SIM</b>   [  ]não':'[  ]Sim   [  ]<b>NÃO</b>');
+            $mtz[] = "<-".$row["valor"]." ";
         } else {
-            $mtz[]="<-".$row['descricao'];
-            $mtz[]="<><div style='display: block'>".($row["ativo"]==1?'[&nbsp;&nbsp;]<b>SIM</b>&nbsp;&nbsp;[&nbsp;&nbsp;]Não':'[&nbsp;&nbsp;]Sim&nbsp;&nbsp;[&nbsp;&nbsp;]<b>NÃO</b>').'</div>';
-            $mtz[]="<-".$o->small($row["valor"]).$o->br(2);
+            $mtz[] = "<-".$row['descricao'];
+            $mtz[] = "<><div style='display: block'>".($row["ativo"]==1?'[&nbsp;&nbsp;]<b>SIM</b>&nbsp;&nbsp;[&nbsp;&nbsp;]Não':'[&nbsp;&nbsp;]Sim&nbsp;&nbsp;[&nbsp;&nbsp;]<b>NÃO</b>').'</div>';
+            $mtz[] = "<-".$o->small($row["valor"]).$o->br(2);
         }
-        $mtz[]="<-";
 
-        $html.=$o->tableRow($mtz, "detail");
+        $mtz[] = "<-";
+
+        $html .= $o->tableRow($mtz, "detail");
     }
+
     $html.=$o->tableEnd();
 
 }
 
-if ($gPage==QUESTIONARIO_RESUMIDO)
-{
+if ($gPage == QUESTIONARIO_RESUMIDO) {
     $o->PDFEnabled = true;
     $o->CSVEnabled = true;
     $o->XLSEnabled = true;
-    $html.=$o->msgTitle("Parâmetros");
-    $html.=$o->msgSubTitle("Questionário resumido para parametrização do sistema");
+    $html .= $o->msgTitle("Parâmetros");
+    $html .= $o->msgSubTitle("Questionário resumido para parametrização do sistema");
 
     $html.=$o->tableBegin("big", true);
-    $mtz = array();
-    $mtz[]="<>Nº";
-    $mtz[]="<-Item                                                 ";
-    $mtz[]="<>Ativar   ";
-    $mtz[]="<-Resposta                            ";
-    $html.=$o->tableRow($mtz, "header");
+    $mtz = [];
+    $mtz[] = "<>Nº";
+    $mtz[] = "<-Item";
+    $mtz[] = "<>Ativar";
+    $mtz[] = "<-Resposta";
+    $html .= $o->tableRow($mtz, "header");
     $cnt = 0;
-    $perguntas = array();
+    $perguntas = [];
     $perguntas['Sistema'][] = "Precisa controlar o saldo por notas fiscais?";
     $perguntas['Sistema'][] = "Imprime usando impressora específica para etiquetas? Qual?";
     $perguntas['Sistema'][] = "Permite ao conferente executar qualquer Pedido (OS) ou só os associados a ele?";
@@ -163,33 +151,32 @@ if ($gPage==QUESTIONARIO_RESUMIDO)
     $perguntas['Inventário'][] = "Usa o inventário simples (UMAs e posições)?";
 
     $grupoAnterior = "";
-    foreach ($perguntas as $grupo => $perguntasDoGrupo)
-    {
-        if ($grupo<>$grupoAnterior)
-        {
+    foreach ($perguntas as $grupo => $perguntasDoGrupo) {
+        if ($grupo !== $grupoAnterior) {
             $grupoAnterior = $grupo;
-            $mtz = array();
+            $mtz = [];
             $mtz[]="~4<>".$grupo;
             $html.=$o->tableRow($mtz, "detail");
         }
-        foreach($perguntasDoGrupo as $pergunta)
-        {
-            $cnt++;
-            $mtz = array();
-            $mtz[]="<>".$cnt;
-            $mtz[]="<-".$pergunta;
-            if ($_REQUEST['gPDF']==1)
-            {
-                $mtz[]="<>[  ]Sim   [  ]Não";
-            } else {
-                $mtz[]="<><div style='display: block'>[&nbsp;&nbsp;]Sim&nbsp;&nbsp;[&nbsp;&nbsp;]Não</div>";
-            }
-            $mtz[]="<-";
 
-            $html.=$o->tableRow($mtz, "detail");
+        foreach($perguntasDoGrupo as $pergunta) {
+            $cnt++;
+            $mtz = [];
+            $mtz[] = "<>".$cnt;
+            $mtz[] = "<-".$pergunta;
+            if ($_REQUEST['gPDF'] == 1) {
+                $mtz[] = "<>[  ]Sim   [  ]Não";
+            } else {
+                $mtz[] = "<><div style='display: block'>[&nbsp;&nbsp;]Sim&nbsp;&nbsp;[&nbsp;&nbsp;]Não</div>";
+            }
+
+            $mtz[] = "<-";
+
+            $html .= $o->tableRow($mtz, "detail");
 
         }
     }
+
     $html.=$o->tableEnd();
 
 }
