f0a36a22638f0c82c9934763fc87d51b2d61c18d
GS-LucasCarvalho <Lucas.carvalho@giusoft.com.br>
2026-01-20 14:19:32 -0300

    Refatora para funcionar no PHP 8.4

--- giusoft/src/view/system/views.php
+++ giusoft/src/view/system/views.php
@@ -1,8 +1,7 @@
-<?
+<?php
+
 include_once $gPathDefault . "gUI.php";
 
 $ui = new gUI("{title: Links; table: geral_relatorios_views; permissions: SIUD; ajax: false; columns: 1}");
 $ui->addDictionary("{name: query; type: code; rows: 10}");
 $ui->run($o, $html);
-
-?>
