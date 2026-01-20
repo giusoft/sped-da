dc674ebf88f9bfb7e256c2694e39aede1b0601cf
GS-LucasCarvalho <Lucas.carvalho@giusoft.com.br>
2026-01-20 14:14:59 -0300

    Refatora para funcionar no PHP 8.4

--- giusoft/src/view/system/locales.php
+++ giusoft/src/view/system/locales.php
@@ -1,11 +1,11 @@
-<?
-$html=$o->msgTitle("Locales");
-$exit=false;
+<?php
+
+$html = $o->msgTitle("Locales");
+$exit = false;
 
 // Ações
-switch ($gPage)
-{
-	//-----------------------------------------------------------------------------------------------
+switch ($gPage) {
+
 	case 0: // Listagem de páginas e opção pra criar uma nova
 		$html.=$o->button("{style: default; icon: plus; title: add; url: " . $o->page . "&gPage=1}");
 		$html.="<br>&nbsp;";
@@ -16,44 +16,44 @@ switch ($gPage)
 		if (count($rs) > 0) {
 			// Obtém os idiomas ativos
 			$tab = '';
-			$tab.=$o->tableBegin('big', true, false, true); // tamanho, borda, zebra, ordenável
-			$mtz = array();
+			$tab .= $o->tableBegin('big', true, false, true); // tamanho, borda, zebra, ordenável
+			$mtz = [];
 			$mtz[] = 'Options';
 			$mtz[] = 'Locale';
 			$mtz[] = '<-Name';
 			$tab.=$o->tableRow($mtz, 'header');
 			foreach ($rs as $field) {
-				$mtz = array();
+				$mtz = [];
 				$btns = '';
-				$btns.= $o->button("{style: danger; size: tiny; icon: trash; hint: Delete; url: " . $o->page . "&gPage=3&gId=" . $field['id'] . "}");
+				$btns .= $o->button("{style: danger; size: tiny; icon: trash; hint: Delete; url: " . $o->page . "&gPage=3&gId=" . $field['id'] . "}");
 				$mtz[] = $btns;
 				$mtz[] = $field['locale'];
 				$mtz[] = '<-<a href="' . $o->page .'&gPage=1&gId=' . $field['id'] . '">'.$field['name'].'</a>';
-				$tab.=$o->tableRow($mtz);
+				$tab .= $o->tableRow($mtz);
 			}
+
 			$tab.=$o->tableEnd();
 		} else {
 			$tab = $o->msgAlert(gT('error_no_fields'));
 		}
-		$html.=$tab;
+
+		$html .= $tab;
 		break;
 
-	//-----------------------------------------------------------------------------------------------
 	case 1: // Novo registro
-		$rs='';
-		$locale=$name='';
-		$pageTitle='New';
-		if ($gId>0)
-		{
-			$pageTitle='Edit';
-			$sql="SELECT p.*
-					FROM gfw_locales p
-					WHERE p.id=$gId";
-			$rs=dbFastQuery($sql);
-			$locale=$rs[0]['locale'];
-			$name=$rs[0]['name'];
+		$rs = '';
+		$locale = '';
+		$name = '';
+		$pageTitle = 'New';
+		if ($gId > 0) {
+			$pageTitle = 'Edit';
+			$sql = "SELECT p.* FROM gfw_locales p WHERE p.id = " . $gId;
+			$rs = dbFastQuery($sql);
+			$locale = $rs[0]['locale'];
+			$name = $rs[0]['name'];
 		}
-		$frm=new gForm("{title: ".$pageTitle."; style: 2column; }");
+
+		$frm = new gForm("{title: ".$pageTitle."; style: 2column; }");
 		$frm->setButtonBackCaption("back");
 
 		$frm->add("{name: gPage; type: hidden; value: 2}");
@@ -61,34 +61,29 @@ switch ($gPage)
 		$frm->add("{name: locale; type: text; value: " . $locale . "}");
 		$frm->add("{name: name; type: text; value: " . $name . "}");
 
-		$html.=$frm->render($o);
+		$html .= $frm->render($o);
 		break;
-	//-----------------------------------------------------------------------------------------------
+
 	case 2: // Tunnel (salvar/editar)
 
 		$flds='';
 		$flds['locale']=gCleanField($_REQUEST['locale']);
 		$flds['name']=gCleanField($_REQUEST['name']);
-		if ($gId == 0)
-		{
+		if ($gId == 0) {
 			$flds['idd']=$usrId;
 			// Insere registro para o idioma informado
 			dbInsert('gfw_locales',$flds);
-		} else
-		{
+		} else {
 			dbUpdate('gfw_locales',$flds,$gId);
 		}
+
 		redirect($o->page);
 		break;
 
-	//-----------------------------------------------------------------------------------------------
+
 	case 3: // Apagar (desativar)
-		$sql="DELETE gfw_locales  WHERE id=$gId";
+		$sql='DELETE gfw_locales  WHERE id = ' . $gId;
 		dbFastQuery($sql);
 		redirect($o->page);
 		break;
 }
-
-
-
-?>
