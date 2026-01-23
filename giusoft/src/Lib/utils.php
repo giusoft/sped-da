933f864f0429ef1af6cbe79b9b4327dbd32512fa
GS-LucasCarvalho <Lucas.carvalho@giusoft.com.br>
2026-01-23 08:26:57 -0300

    Adiciona metodos no utils

--- giusoft/src/Lib/utils.php
+++ giusoft/src/Lib/utils.php
@@ -207,10 +207,42 @@ if (!function_exists("formatarDecimal")) {
 }
 
 
-if (!function_exists("extrairNumeros")) {
-    function extrairNumeros($texto)
+if (!function_exists("removerAcentos")) {
+    function removerAcentos($texto)
     {
-        return preg_replace('/[^0-9]+/i ', '', $texto);
+        $map = [
+            'á' => 'a', 'à' => 'a', 'ã' => 'a', 'â' => 'a', 'ä' => 'a',
+            'é' => 'e', 'è' => 'e', 'ê' => 'e', 'ë' => 'e',
+            'í' => 'i', 'ì' => 'i', 'î' => 'i', 'ï' => 'i',
+            'ó' => 'o', 'ò' => 'o', 'õ' => 'o', 'ô' => 'o', 'ö' => 'o',
+            'ú' => 'u', 'ù' => 'u', 'û' => 'u', 'ü' => 'u',
+            'ç' => 'c', 'ñ' => 'n',
+            'Á' => 'A', 'À' => 'A', 'Ã' => 'A', 'Â' => 'A', 'Ä' => 'A',
+            'É' => 'E', 'È' => 'E', 'Ê' => 'E', 'Ë' => 'E',
+            'Í' => 'I', 'Ì' => 'I', 'Î' => 'I', 'Ï' => 'I',
+            'Ó' => 'O', 'Ò' => 'O', 'Õ' => 'O', 'Ô' => 'O', 'Ö' => 'O',
+            'Ú' => 'U', 'Ù' => 'U', 'Û' => 'U', 'Ü' => 'U',
+            'Ç' => 'C', 'Ñ' => 'N',
+            'º' => '.', '°' => '.', 'ª' => '.', '&' => 'e'
+        ];
+
+        $texto = strtr($texto, $map);
+        $texto = preg_replace("/[^a-zA-Z0-9\s\-\.,;:\/]/", "", $texto);
+
+        return trim($texto);
+    }
+}
+
+
+if (!function_exists("removerEstranhos")) {
+    function removerEstranhos($texto)
+    {
+        $texto = removerAcentos($texto);
+        return preg_replace(
+            '/[^a-z0-9\+\-\=\.\,\!\?\:\;\@\%\&\(\)\{\}\<\>\[\]\s\'\$\/]+/i ',
+            '',
+            $texto
+        );
     }
 }
 
